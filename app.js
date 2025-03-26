40e532576f151dae3a68df2a8fa071df024872cb
mohitksingh19 <mohit.k.singh@globallogic.com>
2025-03-26 17:36:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.get('/weather', async (req, res) => {
 });
 
 // Start the server
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
