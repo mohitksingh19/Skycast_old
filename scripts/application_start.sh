413f917939d5adc6093e7986adcf348d4bc90a14
mohitksingh19 <mohit.k.singh@globallogic.com>
2025-03-27 14:36:15 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'run application_start.sh: ' >> /home/ec2-user/Skycast/deploy.log
 echo 'pm2 restart Skycast' >> /home/ec2-user/Skycast/deploy.log
-pm2 restart 'Skycast' >> /home/ec2-user/Skycast/deploy.log
+pm2 restart 'nodejs-Skycast' >> /home/ec2-user/Skycast/deploy.log
