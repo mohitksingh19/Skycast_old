ff38f463454a88162a04314a1071b73db7db86ae
mohitksingh19 <mohit.k.singh@globallogic.com>
2025-04-06 09:42:09 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+APP_NAME="nodejs-Skycast"
+
+# Check if the application is registered in PM2
 echo 'run application_start.sh: ' >> /home/ec2-user/Skycast/deploy.log
 echo 'pm2 restart Skycast' >> /home/ec2-user/Skycast/deploy.log
-pm2 restart 'nodejs-Skycast' >> /home/ec2-user/Skycast/deploy1.log
+if pm2 list | grep -qw "$APP_NAME"; then
+  pm2 restart 'nodejs-Skycast' >> /home/ec2-user/Skycast/deploy1.log
+else
+  pm2 start npm --name=nodejs-Skycast -- start
+fi
